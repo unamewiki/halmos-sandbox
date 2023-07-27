8cfeb90bedb6dc271e88c1136f6dd2186a9ed38a
karmacoma <karma@coma.lol>
2023-07-27 14:54:32 -0700

    More examples

--- test/5_anycallLowLevel.t.sol
+++ test/5_anycallLowLevel.t.sol
@@ -3,7 +3,35 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 
 // import Halmos cheatcodes
-import {SymTest} from "halmos-cheatcodes/SymTest.sol";
+// import {SymTest} from "halmos-cheatcodes/SymTest.sol";
+
+/// @notice Symbolic Virtual Machine
+interface SVM {
+    // Create a new symbolic uint value ranging over [0, 2**bitSize - 1] (inclusive)
+    function createUint(uint256 bitSize, string memory name) external returns (uint256 value);
+
+    // Create a new symbolic byte array with the given byte size
+    function createBytes(uint256 byteSize, string memory name) external returns (bytes memory value);
+
+    // Create a new symbolic uint256 value
+    function createUint256(string memory name) external returns (uint256 value);
+
+    // Create a new symbolic bytes32 value
+    function createBytes32(string memory name) external returns (bytes32 value);
+
+    // Create a new symbolic address value
+    function createAddress(string memory name) external returns (address value);
+
+    // Create a new symbolic boolean value
+    function createBool(string memory name) external returns (bool value);
+}
+
+abstract contract SymTest {
+    // SVM cheat code address: 0xf3993a62377bcd56ae39d773740a5390411e8bc9
+    address internal constant SVM_ADDRESS = address(uint160(uint256(keccak256("svm cheat code"))));
+
+    SVM internal constant svm = SVM(SVM_ADDRESS);
+}
 
 contract MockERC20 {
 
@@ -56,6 +84,8 @@ contract Test5 is Test, SymTest {
         assertEq(balanceBefore, balanceAfter);
     }
 
+    /// finds the bug and generates a counterexample:
+    ///     halmos_data_bytes_01 = 0x30e0789e00000000000000000000000000000000000000000000000000000000aaaa000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004
     function test_anycallLowLevelExplicit() external {
         bytes memory data = svm.createBytes(100, 'data');
         uint256 balanceBefore = token.balanceOf(owner);
---
+++ test/7_BadMax.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+import "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+
+/// @notice examples are from the talk "Program Synthesis: A Dream Realized?" by Roopsha Samanta
+contract Test7 is Test {
+    function badMax(uint256 x, uint256 y, uint256 z) public pure returns (uint256 max) {
+        max = x;
+        if (y < z) max = z;
+        if (max < y) max = y;
+    }
+
+    function goodMax(uint256 x, uint256 y, uint256 z) public pure returns (uint256 max) {
+        max = z;
+        if (z <= y) max = y;
+        if (max < x) max = x;
+    }
+
+    function assertMaxProperties(uint256 max, uint256 x, uint256 y, uint256 z) internal {
+        assertTrue(max == x || max == y || max == z);
+        assertGe(max, x);
+        assertGe(max, y);
+        assertGe(max, z);
+    }
+
+    function test_badMax(uint256 x, uint256 y, uint256 z) public {
+        uint256 max = badMax(x, y, z);
+        assertMaxProperties(max, x, y, z);
+    }
+
+    function test_goodMax(uint256 x, uint256 y, uint256 z) public {
+        uint256 max = goodMax(x, y, z);
+        assertMaxProperties(max, x, y, z);
+    }
+}
