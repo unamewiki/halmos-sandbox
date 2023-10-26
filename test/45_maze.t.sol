f6fe56a06c5bf3f5cc892c9ce4fa2aa0424684da
karmacoma <karma@coma.lol>
2023-10-26 11:27:02 -0700

    add Vat test

---
+++ test/45_maze.t.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+
+import {LibString} from "solmate/utils/LibString.sol";
+
+
+contract Test45 is Test {
+    enum Move {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    bool constant DEBUG = true;
+    uint256 constant SAMPLE_RATE = 24;
+    uint256 currentSample = SAMPLE_RATE;
+
+    function info(bytes[] memory maze, uint256 num, string memory s) public {
+        if (DEBUG) {
+            // if (currentSample != 0) {
+            //     currentSample--;
+            //     return;
+            // }
+
+            // currentSample = SAMPLE_RATE;
+
+            string memory maze_str = string.concat(
+                "        ", string(maze[0]),
+                "        ", string(maze[1]),
+                "        ", string(maze[2]),
+                "        ", string(maze[3]),
+                "        ", string(maze[4])
+                // "        ", string(maze[5]),
+                // "        ", string(maze[6]),
+                // "        ", string(maze[7]),
+                // "        ", string(maze[8]),
+                // "        ", string(maze[9])
+            );
+
+            // clear screen
+            string memory CLEAR_SCREEN = unicode"[2J";
+            string memory message = string.concat(
+                CLEAR_SCREEN,
+                unicode"\n    ",
+                LibString.toString(num),
+                " moves // ",
+                s,
+                unicode"\n\n",
+                maze_str
+            );
+
+            console2.log(message);
+        }
+    }
+
+    function render_maze(bytes[] memory maze) public pure {
+        unchecked {
+            uint256 length = maze.length;
+            for (uint256 i = 0; i < length; i++) {
+                console2.log(string(maze[i]));
+            }
+        }
+    }
+
+    /// @custom:halmos --loop=12 --statistics
+    function test_maze(uint8[] calldata moves) external {
+        /// @dev hard maze, 18 moves, dead ends, multiple solutions
+        // starting position:
+        // uint256 x = 0;
+        // uint256 y = 4;
+
+        // bytes[] memory maze = new bytes[](10);
+        // maze[0] = unicode"###########\n";
+        // maze[1] = unicode"#   #     #\n";
+        // maze[2] = unicode"# # # # # #\n";
+        // maze[3] = unicode"### ### ###\n";
+        // maze[4] = unicode"S #   #   E\n";
+        // maze[5] = unicode"# # # ### #\n";
+        // maze[6] = unicode"#   #     #\n";
+        // maze[7] = unicode"##### # ###\n";
+        // maze[8] = unicode"#     #   #\n";
+        // maze[9] = unicode"###########\n";
+
+        /// @dev medium maze, 16 moves
+        // starting position:
+        // uint256 x = 0;
+        // uint256 y = 1;
+
+        bytes[] memory maze = new bytes[](5);
+        maze[0] = unicode"#########\n";
+        maze[1] = unicode"S #   # E\n";
+        maze[2] = unicode"# # # # #\n";
+        maze[3] = unicode"#   #   #\n";
+        maze[4] = unicode"#########\n";
+
+        /// @dev easy maze, 12 moves
+        // starting position:
+        uint256 x = 0;
+        uint256 y = 1;
+
+        console2.log("moves.length =", moves.length);
+        uint256 maze_width = maze[0].length;
+        uint256 maze_height = maze.length;
+
+        unchecked {
+            for (uint256 i = 0; i < moves.length; i++) {
+                // for foundry
+                uint8 move = moves[i] % 4;
+                uint256 n = i + 1;
+
+                // update player position with current move
+                if (move == uint8(Move.RIGHT)) {
+                    if (x == maze_width) {
+                        return info(maze, n, unicode"❌ x > maze_width");
+                    }
+                    x++;
+                } else if (move == uint8(Move.DOWN)) {
+                    if (y == maze_height) {
+                        return info(maze, n, unicode"❌ y > maze_height");
+                    }
+                    y++;
+                } else if (move == uint8(Move.LEFT)) {
+                    if (x == 0) {
+                        return info(maze, n, unicode"❌ x < 0");
+                    }
+                    x--;
+                } else if (move == uint8(Move.UP)) {
+                    if (y == 0) {
+                        return info(maze, n, unicode"❌ y < 0");
+                    }
+                    y--;
+                } else {
+                    return info(maze, n, unicode"❌ invalid move");
+                }
+
+                if (maze[y][x] == "#") {
+                    maze[y][x] = "X";
+                    return info(maze, n, unicode"❌ hit wall");
+                } else if (maze[y][x] == "E") {
+                    // we did it 🙌
+                    info(maze, n, unicode"🏁 reached the end!");
+                    assert(false);
+                } else {
+                    maze[y][x] = ".";
+                    info(maze, n, unicode"🐿️  searching");
+                }
+            }
+        }
+    }
+}
---
+++ test/HalmosToken.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {SymTest} from "halmos-cheatcodes/SymTest.sol";
+import {ERC20} from "solady/src/tokens/ERC20.sol";
+import {Ownable} from "solady/src/auth/Ownable.sol";
+
+contract HalmosToken is ERC20, Ownable {
+    function name() public view override returns (string memory) {
+        return "HalmosToken";
+    }
+
+    /// @dev Returns the symbol of the token.
+    function symbol() public view override returns (string memory) {
+        return "HT";
+    }
+
+    function initialize() public {
+        _initializeOwner(msg.sender); // No _guardInitializeOwner()
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
+
+/// @custom:halmos --storage-layout=generic
+contract HalmosTokenTest is SymTest, Test {
+    HalmosToken token;
+    address owner;
+
+    function setUp() public {
+        owner = svm.createAddress("owner");
+        vm.startPrank(owner);
+
+        uint256 initialSupply = svm.createUint256("initialSupply");
+        vm.assume(initialSupply < 10);  // This is to help make debugging easier but you can remove it.
+
+        token = new HalmosToken();
+        token.initialize();
+        token.mint(owner, initialSupply);
+    }
+
+    function check_0xlgtm_transfer() public {
+        // specify input conditions
+        address sender = svm.createAddress("sender");
+        address receiver = svm.createAddress("receiver");
+        uint256 senderBalance = svm.createUint256("senderBalance");
+        uint256 receiverBalance = svm.createUint256("receiverBalance");
+        uint256 amount = svm.createUint256("amount");
+
+        // Assumptions
+        uint256 totalSupply = token.totalSupply();
+        vm.assume(sender != address(0));
+        vm.assume(receiver != address(0));
+        // vm.assume(sender != owner);
+        // vm.assume(receiver != owner);
+
+        vm.assume(senderBalance + receiverBalance <= totalSupply);
+        vm.assume(amount <= senderBalance);
+        vm.assume(receiverBalance + amount <= totalSupply); // maybe redundant?
+        vm.assume(sender != receiver);
+
+        // Call target contract
+        vm.startPrank(owner);
+        token.transfer(sender, senderBalance);
+        token.transfer(receiver, receiverBalance);
+        vm.stopPrank();
+
+        vm.prank(sender);
+        token.transfer(receiver, amount);
+
+        // check output state
+        assertEq(token.balanceOf(sender), senderBalance - amount);
+        assertEq(token.balanceOf(receiver), receiverBalance + amount);
+    }
+}
